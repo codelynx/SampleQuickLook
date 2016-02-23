4d34b9f493da28aae52c80340a3adf7f91f45cd3
Kaz Yoshikawa <kaz@digitallynx.com>
2016-02-23 18:12:32 +0900

    initial check in

---
+++ SampleQuickLook_Mac/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SampleQuickLook_Mac
+//
+//  Created by Kaz Yoshikawa on 2/23/16.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
